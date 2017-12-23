c70f25e196415fb64314fcb52e5334ac0fb74408
Hema vathi <eventurersmac4@ev-mac6.local>
2017-12-23 10:22:01 +0530

    Initial Commit

---
+++ GetFactors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GetFactors
+//
+//  Created by ev_mac6 on 23/12/17.
+//  Copyright © 2017 ev_mac6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
